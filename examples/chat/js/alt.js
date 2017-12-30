6d34af1e47ffc172a78559f238326e3f0419b07b
bkniffler@me.com <bkniffler@me.com>
2017-12-30 22:29:09 +0000

    Deploy from Forestry.io

--- examples/chat/js/alt.js
+++
@@ -1,8 +0,0 @@
-var Alt = require('../../../')
-var alt = new Alt()
-
-var DispatcherRecorder = require('../../../utils/DispatcherRecorder')
-
-window.alt = alt
-window.DispatcherRecorder = DispatcherRecorder
-module.exports = alt
